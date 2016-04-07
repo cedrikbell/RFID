2f7fae10be429b37aed8a38c88dbf7494789bd25
Cedrik Bell <cedrik.bell@usma.edu>
2016-04-07 11:08:46 -0400

    Update session.php

--- src/session.php
+++ src/session.php
@@ -1,22 +1,18 @@
-
 <?php
-error_reporting(E_ALL ^ E_DEPRECATED);
 
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysql_connect("localhost", "root", "sqldba");
-// Selecting Database
-$db = mysql_select_db("RFID_Database", $connection);
+$connection = mysqli_connect('localhost','developer','cisco123','RFID_Database');
 session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$ses_sql=mysql_query("select userName from users where userName='$user_check'", $connection);
-$row = mysql_fetch_assoc($ses_sql);
+$ses_sql=mysqli_query($connection,"select userName from users where userName='$user_check'");
+$row = mysqli_fetch_assoc($ses_sql);
 $login_session =$row['userName'];
 if(!isset($login_session)){
-mysql_close($connection); // Closing Connection
+mysqli_close($connection); // Closing Connection
 header('Location: index.php'); // Redirecting To Home Page
 }
 ?>
 
-<!-- Agarwal, Nareej. "PHP Login Form with Sessions." PHP Login Form with Sessions. Formget, n.d. Web. 26 Feb. 2016. <http://www.formget.com/login-form-in-php/> -->
+<!-- Agarwal, Nareej. "PHP Login Form with Sessions." PHP  Login Form with Sessions. Formget, n.d. Web. 26 Feb. 2016. <http://www.formget.com/login-form-in-php/> -->
